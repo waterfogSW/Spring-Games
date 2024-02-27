96e7a136d02cb0d9a3ee195bc8baad992dc94ea8
San Kim <waterfog9580@gmail.com>
2024-02-27 21:45:08 +0900

    [#1] 🎉 프로젝트 초기 설정 (#2)

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,18 @@
+object Version {
+
+    const val JAVA = "21"
+    const val KOTLIN = "1.9.22"
+    const val SPRING_BOOT = "3.2.3"
+    const val SPRING_BOOT_DEPENDENCY_MANAGEMENT = "1.1.4"
+
+    const val UUID_CREATOR = "5.3.3"
+    const val MOCKK = "1.13.7"
+    const val SPRING_MOCKK = "4.0.2"
+    const val KOTEST = "5.6.0"
+    const val KOTEST_EXTENSIONS_SPRING = "1.1.3"
+    const val JACKSON = "2.16.1"
+    const val MYSQL = "8.0.33"
+    const val H2 = "2.2.224"
+    const val KOTLIN_LOGGING = "5.1.0"
+
+}
