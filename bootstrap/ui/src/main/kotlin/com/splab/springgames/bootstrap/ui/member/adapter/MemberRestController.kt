cb13f727ceff9f5a862728ce436e36a5dbff9e0b
San Kim <waterfog9580@gmail.com>
2024-02-29 13:51:31 +0900

    [#24] ✨ 회원 수정 API 구현

--- bootstrap/ui/src/main/kotlin/com/splab/springgames/bootstrap/ui/member/adapter/MemberRestController.kt
+++ bootstrap/ui/src/main/kotlin/com/splab/springgames/bootstrap/ui/member/adapter/MemberRestController.kt
@@ -1,19 +1,25 @@
 package com.splab.springgames.bootstrap.ui.member.adapter
 
+import com.splab.springgames.application.member.port.inbound.EditMemberUseCase
 import com.splab.springgames.application.member.port.inbound.EnrollMemberUseCase
+import com.splab.springgames.bootstrap.ui.member.dto.EditMemberRequest
 import com.splab.springgames.bootstrap.ui.member.dto.EnrollMemberRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/members")
-class MemberRestController (
+class MemberRestController(
     private val enrollMemberUseCase: EnrollMemberUseCase,
-){
+    private val editMemberUseCase: EditMemberUseCase,
+) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,6 +30,19 @@ class MemberRestController (
         request
             .toCommand()
             .also { enrollMemberUseCase.invoke(it) }
+    }
+
+    @PutMapping("/{memberId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun editMember(
+        @PathVariable
+        memberId: UUID,
+        @RequestBody
+        request: EditMemberRequest
+    ) {
+        request
+            .toCommandWith(memberId)
+            .also { editMemberUseCase.invoke(it) }
     }
 
 }
---
+++ bootstrap/ui/src/main/kotlin/com/splab/springgames/bootstrap/ui/member/dto/EditMemberRequest.kt
@@ -0,0 +1,65 @@
+package com.splab.springgames.bootstrap.ui.member.dto
+
+import com.splab.springgames.application.member.port.inbound.EditMemberUseCase
+import com.splab.springgames.domain.member.exception.MemberExceptionType
+import com.splab.springgames.support.common.exception.CustomException
+import java.time.LocalDate
+import java.util.*
+
+
+data class EditMemberRequest(
+    val name: String?,
+    val email: String?,
+    val registeredDate: LocalDate?,
+) {
+
+    fun toCommandWith(memberId: UUID): EditMemberUseCase.Command {
+        return EditMemberUseCase.Command(
+            memberId = getIdOrThrow(memberId),
+            name = getNameOrThrow(name),
+            email = getEmailOrThrow(email),
+            registeredDate = getRegisteredDateOrThrow(registeredDate)
+        )
+    }
+
+    private fun getIdOrThrow(id: UUID?): UUID {
+        if (id == null) {
+            throw CustomException(
+                type = MemberExceptionType.INVALID_ID_INPUT,
+                message = "ID는 필수 입력값입니다."
+            )
+        }
+        return id
+    }
+
+    private fun getNameOrThrow(name: String?): String {
+        if (name.isNullOrBlank()) {
+            throw CustomException(
+                type = MemberExceptionType.INVALID_NAME_INPUT,
+                message = "이름은 필수 입력값입니다."
+            )
+        }
+        return name
+    }
+
+    private fun getEmailOrThrow(email: String?): String {
+        if (email.isNullOrBlank()) {
+            throw CustomException(
+                type = MemberExceptionType.INVALID_EMAIL_INPUT,
+                message = "이메일은 필수 입력값입니다."
+            )
+        }
+        return email
+    }
+
+    private fun getRegisteredDateOrThrow(registeredDate: LocalDate?): LocalDate {
+        if (registeredDate == null) {
+            throw CustomException(
+                type = MemberExceptionType.INVALID_REGISTER_DATE_INPUT,
+                message = "가입일은 필수 입력값입니다."
+            )
+        }
+        return registeredDate
+    }
+
+}
