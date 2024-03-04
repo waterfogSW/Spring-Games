63277a296b30aa0d92c715d51070cc0f879d4a37
San Kim <waterfog9580@gmail.com>
2024-03-05 00:45:14 +0900

    [#48] 🐛 회원 수정시 이미 사용중인 회원 이메일 예외 처리

--- application/src/main/kotlin/com/splab/springgames/application/member/service/command/EditMemberService.kt
+++ application/src/main/kotlin/com/splab/springgames/application/member/service/command/EditMemberService.kt
@@ -2,8 +2,13 @@ package com.splab.springgames.application.member.service.command
 
 import com.splab.springgames.application.member.port.inbound.EditMemberUseCase
 import com.splab.springgames.application.member.port.outbound.MemberRepository
+import com.splab.springgames.domain.member.domain.Member
+import com.splab.springgames.domain.member.exception.MemberExceptionType
+import com.splab.springgames.domain.member.vo.Email
+import com.splab.springgames.support.common.exception.CustomException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
 @Service
 class EditMemberService(
@@ -12,14 +17,31 @@ class EditMemberService(
 
     @Transactional
     override fun invoke(command: EditMemberUseCase.Command) {
-        memberRepository
-            .getById(command.memberId)
-            .update(
-                name = command.name,
-                email = command.email,
-                registeredDate = command.registeredDate
-            )
-            .also { memberRepository.save(it) }
+        val member: Member = memberRepository.getById(command.memberId)
+
+        validateEmailUpdatable(member.id, command.email)
+
+        member.update(
+            email = command.email,
+            name = command.name,
+            registeredDate = command.registeredDate,
+        ).also { memberRepository.save(it) }
     }
+
+    private fun validateEmailUpdatable(
+        targetMemberId: UUID,
+        emailString: String
+    ) {
+        val email: Email = Email.create(emailString)
+        val emailMember: Member? = memberRepository.findByEmail(email)
+
+        if (emailMember != null && emailMember.id != targetMemberId) {
+            throw CustomException(
+                type = MemberExceptionType.DUPLICATED_EMAIL_INPUT,
+                message = "이미 사용중인 이메일입니다.",
+            )
+        }
+    }
+
 
 }
--- application/src/test/kotlin/com/splab/springgames/application/member/service/command/EditMemberServiceTest.kt
+++ application/src/test/kotlin/com/splab/springgames/application/member/service/command/EditMemberServiceTest.kt
@@ -4,9 +4,12 @@ import com.splab.springgames.application.member.port.inbound.EditMemberUseCase
 import com.splab.springgames.application.member.port.outbound.MemberRepositorySpy
 import com.splab.springgames.domain.member.MemberFixtureFactory
 import com.splab.springgames.domain.member.domain.Member
+import com.splab.springgames.domain.member.exception.MemberExceptionType
 import com.splab.springgames.domain.member.vo.Email
 import com.splab.springgames.domain.member.vo.Name
 import com.splab.springgames.domain.member.vo.RegisteredDate
+import com.splab.springgames.support.common.exception.CustomException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
@@ -47,6 +50,34 @@ class EditMemberServiceTest : DescribeSpec({
             updatedMember.name shouldBe Name(updateName)
             updatedMember.email shouldBe Email(updateEmail)
             updatedMember.registeredDate shouldBe RegisteredDate(updateRegisteredDate)
+        }
+
+        context("[실패] 다른 유저가 이미 사용중인 이메일로 수정하면") {
+            it("CustomException, DUPLICATED_EMAIL_INPUT 예외를 발생시킨다.") {
+                // arrange
+                val existsEmail = "test@test.com"
+                val targetEmail = "test@naver.com"
+                val existsMember: Member = MemberFixtureFactory
+                    .create(email = existsEmail)
+                    .also { memberRepositorySpy.save(it) }
+
+                MemberFixtureFactory
+                    .create(email = targetEmail)
+                    .also { memberRepositorySpy.save(it) }
+
+                val command = EditMemberUseCase.Command(
+                    memberId = existsMember.id,
+                    name = "수정된 이름",
+                    email = targetEmail,
+                    registeredDate = LocalDate.now()
+                )
+
+                // act && assert
+                val exception = shouldThrow<CustomException> {
+                    sut.invoke(command)
+                }
+                exception.type shouldBe MemberExceptionType.DUPLICATED_EMAIL_INPUT
+            }
         }
     }
 
